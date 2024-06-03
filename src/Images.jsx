52b22792b3cffa52ae9cf8744ee82ccd3e392fcb
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-06-03 11:59:40 -0400

    breaking for lunch

---
+++ src/Images.jsx
@@ -0,0 +1,27 @@
+import {useState, useContext, useEffect } from "react"
+import { AuthContext } from './context'
+import { getImages } from "./api"
+
+const Images = () => {
+    const [images, setImages] = useState([])
+    const { auth } = useContext(AuthContext)
+
+    useEffect(() => {
+        if (auth.accessToken){
+            getImages({ auth })
+                .then(response => {
+                    console.log('GET IMAGES: RESPONSE: ', response)
+                    setImages(response.data)
+                })
+                .catch(error => console.log('ERROR: ', error))
+        }
+    }, [auth.accessToken])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Images
